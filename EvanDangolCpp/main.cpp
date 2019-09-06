8dc572b3231ef748ebf14baa03faa84b98ebbdb6
Evan Sir <dangolevan@gmail.com>
2019-09-06 11:29:34 +0545

    t6756

--- EvanDangolCpp/main.cpp
+++ EvanDangolCpp/main.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 #include "simant/nestedloop.h"
+#include "simant/functions.h"
 using namespace std;
 
 int main()
 {
-	nestedlooprun();
+	add();
 
 }
---
+++ EvanDangolCpp/simant/functions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void add()
+{
+	int a = 5;
+	int b = 10;
+	int c = a + b;
+	cout << c;
+}
