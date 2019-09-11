982521c7e4a8c0ee13173bf3463f2dd23859b48b
Evan Sir <dangolevan@gmail.com>
2019-09-11 12:06:37 +0545

    55

--- EvanDangolCpp/main.cpp
+++ EvanDangolCpp/main.cpp
@@ -3,5 +3,12 @@
 #include<string>
 using namespace evan;
 using namespace simant;
-
+#include "evan/EvanWindow.h"
+#ifdef _MSC_VER
+#    pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+int main()
+{
+	WinMain(0,0,0,0);
+}
 
