9b688b616c56ed4c531f47e11edfcb309b4e48cd
Evan Sir <dangolevan@gmail.com>
2019-09-12 11:02:05 +0545

    ertyer

--- EvanDangolCpp/main.cpp
+++ EvanDangolCpp/main.cpp
@@ -3,12 +3,47 @@
 #include<string>
 using namespace evan;
 using namespace simant;
-#include "evan/EvanWindow.h"
-#ifdef _MSC_VER
-#    pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
-#endif
+
+
 int main()
 {
-	WinMain(0,0,0,0);
+	char ch[] = { 'a','b','x','y','e' };
+
+	char ch1[] = "simant";
+
+	const char* x = "hello world";
+
+	cout << *x;
+	char* cp = ch1;
+
+	//cp = cp + 3;
+	//cout <<*cp << endl;
+
+	int count = 0;
+	while (*cp != '\0')
+	{
+		count++;
+		cp++;
+	}
+	//cout << count;
+
+
+	int arr[] = { 31,16,60,20,32,77,12,19 };
+	int* p = arr;
+
+
+	p = p + 5;
+//	cout << *p;
+	/*for (size_t i = 0; i < 8; i++)
+	{
+		if (*p >= 16)
+		{
+			cout << *p << endl;
+		}
+		p++;
+	}*/
+	
+
+	
 }
 
