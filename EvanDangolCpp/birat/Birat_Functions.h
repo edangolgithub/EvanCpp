feb83ad7d85d6f2610f8a8ec5248f3366bf2c48e
Evan Sir <dangolevan@gmail.com>
2019-09-17 11:43:46 +0545

    commit

--- EvanDangolCpp/birat/Birat_Functions.h
+++ EvanDangolCpp/birat/Birat_Functions.h
@@ -1,1 +1,44 @@
 #pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+namespace birat
+{
+	void add(int a, int b,int c,string x)
+	{
+		cout << a + b + c << endl;
+		cout << x;
+	}
+
+	int makedouble(int x)
+	{
+		return x * 2;
+	}
+
+	void subt(string a, string b)
+	{
+		cout << a + b << endl;
+		cout << b;
+	}
+
+	int makesmall(int b)
+	{
+		return b / 3;
+	}
+	void myfunt(string fname)
+	{
+		cout << fname << "grg.\n";
+	}
+	void my_age(string fname, int age)
+	{
+		cout << fname << "gurung." << age << "years old.\n";
+	}
+	int mixed(int d,int e)
+	{
+		return d - e;
+	}
+	
+	
+}
