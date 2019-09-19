6a5c43be9c6c24a44fdfabd2ea25e82676adb7da
edangol <edangol@DESKTOP-5Q5LBOF>
2019-09-19 22:07:50 +0545

    34

--- EvanDangolCpp/evan/Filehandling.h
+++ EvanDangolCpp/evan/Filehandling.h
@@ -1,22 +1,33 @@
 #pragma once
 #include<iostream>
 #include<fstream>
+#include<conio.h>
 using namespace std;
 namespace evan
 {
 	void read()
 	{
-		fstream f("EvanDangolCpp/evan/data/abc.txt", ios::in);
+		fstream f("data/abc.txt", ios::in);
 		if (f.fail())
 		{
 			cout << "error";
+			_getch();
 			exit(100);
 		}
+		char * cp = (char *)malloc(sizeof(char));
 		char buf;
+		int count = 0;
 		while (f.get(buf))
 		{
-			cout << buf;
+			cp[count] = buf;
+			count++;
+			cp = (char *)realloc(cp, sizeof(char)*count+1);
+
+
 		}
+		cp[count] = '\0';
+		cout << cp;
 		f.close();
+		_getch();
 	}
 }
