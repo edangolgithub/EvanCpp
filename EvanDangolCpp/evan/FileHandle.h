c647b1a53c5c7c4d439bf197c0b0d60d1f029f38
Evan Sir <dangolevan@gmail.com>
2019-09-19 13:49:43 +0545

    gggg

---
+++ EvanDangolCpp/evan/FileHandle.h
@@ -0,0 +1,23 @@
+//#pragma once
+//#ifdef EVANDANGOLCPP_EXPORTS
+//#define EVANDANGOLCPP_API __declspec(dllexport)
+//#else
+//#define EVANDANGOLCPP_API __declspec(dllimport)
+//#endif
+//#include <fstream>
+//#include<iostream>
+//using namespace std;
+//class EVANDANGOLCPP_API FileHandle
+//{
+//public:
+//	fstream * file;
+//	FileHandle();
+//	~FileHandle();
+//
+//	void WriteFile();
+//	void ReadFile();
+//
+//	const char* GetCin();
+//
+//	string returnstring();
+//};
