9861ebd00520cb2414fde1a058e6495a5f17e693
Evan Sir <dangolevan@gmail.com>
2019-09-20 12:20:42 +0545

    microsoft merge

--- EvanDangolCpp/birat/birat_class.h
+++ EvanDangolCpp/birat/birat_class.h
@@ -77,3 +77,57 @@ using namespace std;
 			cout << "owner's name:" << endl;
 		}
 	};
+
+	void runclass()
+	{
+		animal abc;
+		abc.legs = 12;
+		strcpy(abc.name, "tommy");
+		strcpy(abc.color, "black");
+		abc.displayanimalk();
+
+
+		animal def;
+		def.legs = 4;
+		strcpy(def.name, "jack");
+		strcpy(def.color, "grey");
+		def.displayanimalk();
+
+
+
+		//#pragma region xyz
+		//	animal myanimal;
+		//	
+		//	myanimal.sleep();
+		//
+		//	myanimal.sleep(); myanimal.sleep(); myanimal.sleep(); myanimal.sleep(); myanimal.sleep(); myanimal.sleep(); myanimal.sleep();
+		//	myanimal.speak();
+		//
+		//	myanimal.speak();
+		//	myanimal.legs = 5;
+		//	myanimal.getlefofanimal();
+		//
+		//
+		//
+		//	animal youranimal;
+		//	youranimal.legs = 17;
+		//	youranimal.getlefofanimal();
+		//#pragma endregion
+
+		bird ghi;
+		ghi.age = 14;
+		strcpy(ghi.color, " white");
+		ghi.wingspan = 13;
+		strcpy(ghi.name, "hawk");
+
+		//ghi.fly();
+		ghi.displayanimalk();
+
+
+		/*car suzuki;
+		strcpy(suzuki.name, "allas");
+		suzuki.owner();*/
+
+
+
+	}
--- EvanDangolCpp/main.cpp
+++ EvanDangolCpp/main.cpp
@@ -2,59 +2,6 @@
 
 
 int main() {
+	cout << "hello";
 	
-	animal abc;
-	abc.legs = 12;
-    strcpy(abc.name ,"tommy");
-	strcpy(abc.color, "black");
-	abc.displayanimalk();
-	
-	
-	animal def;
-	def.legs = 4;
-	strcpy(def.name, "jack");
-	strcpy(def.color, "grey");
-	def.displayanimalk();
-	
-
-
-//#pragma region xyz
-//	animal myanimal;
-//	
-//	myanimal.sleep();
-//
-//	myanimal.sleep(); myanimal.sleep(); myanimal.sleep(); myanimal.sleep(); myanimal.sleep(); myanimal.sleep(); myanimal.sleep();
-//	myanimal.speak();
-//
-//	myanimal.speak();
-//	myanimal.legs = 5;
-//	myanimal.getlefofanimal();
-//
-//
-//
-//	animal youranimal;
-//	youranimal.legs = 17;
-//	youranimal.getlefofanimal();
-//#pragma endregion
-
-	bird ghi;
-	ghi.age = 14;
-	strcpy(ghi.color," white");
-	ghi.wingspan = 13;
-    strcpy(	ghi.name , "hawk");
-
-	//ghi.fly();
-	ghi.displayanimalk();
-
-
-	/*car suzuki;
-	strcpy(suzuki.name, "allas");
-	suzuki.owner();*/
-
-
-
-
-
-
-
 }
