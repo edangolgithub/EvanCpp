0c1d1bb6b4853ba7a8fb460fdec29d85c9241a5f
Evan Sir <dangolevan@gmail.com>
2019-09-20 12:22:53 +0545

    dfgdf

--- EvanDangolCpp/main.cpp
+++ EvanDangolCpp/main.cpp
@@ -1,13 +1,5 @@
-#include <iostream>
-#include <windows.h>
-#include<stdio.h>
-#include "evan/EvanDllWrapper.h"
-#include "simant/FunctionPointer.h"
 #include"birat/birat_array.h"
-using namespace evan;
-using namespace simant;
-using namespace std;
 
 int main() {
-	array1();
+	cout << "bye";
 }
