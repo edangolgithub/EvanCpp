7e942caecfb219f24d639d3515d6f8719d177c9e
edangol <edangol@EVAN1>
2020-01-09 13:56:52 +0545

    bhb h

--- EvanDangolCpp/ribesh/structure.cpp
+++ EvanDangolCpp/ribesh/structure.cpp
@@ -1,36 +1,25 @@
 #include<iostream>
 using namespace std;
-
 struct student
 {
 	int age;
 	float roll;
 	const char *name;
 };
-
 union foo
 {
  int x;
  int y;
  char ch;
 };
-
-
-
-
-
 void gjhfg()
-{
-
-	student s;
-	
+{   student s;
 	s.age=40;
 	s.roll=45;
 	s.name="ribesh";
 	cout<<s.name<<endl;
 	cout<<s.age<<endl;
 	cout<<s.roll<<endl;
-	
 	foo f;
 f.x = 65;
 cout<<f.ch;
